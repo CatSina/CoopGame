d8ac93bdbef92b53d96cc2cd7ca3df0af35b1658
catsina <sinaghavamy.sg79@gmail.com>
2020-08-11 17:36:24 +0430

    Coop
    doing this for a while but now I am going to use git

---
+++ Source/CoopGame/CoopGame.h
@@ -0,0 +1,10 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#define SURFACE_FLESHDEFAULT		SurfaceType1
+#define SURFACE_FLESVULNERABLE		SurfaceType2
+
+#define COLLISION_WEAPOM			ECC_GameTraceChannel1
